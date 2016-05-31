bfb1167ad85e2994ca013e52d3f5159bdbb6a126
vvanghelue <vincent.vanghelue@gmail.com>
2016-05-31 14:11:53 +0200

    first commit

---
+++ example.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+var digitsClient = require('./index');
+//var digitsClient = require('digits-server-client');
+
+var digits = new digitsClient({
+	digitsConsumerKey: 'myConsumerKey',
+	digitsHost: "https://mydomain.com"
+});
+
+digits.sendVerificationCode({
+	phoneNumber: '0648446907',
+	countryCode: 'FR'
+}).then(function (registrationToken) {
+	//eyJsb2dpblZlcmlmaWNhdGlvblJlcXVlc3RJZCI6InV...
+	console.log(registrationToken);
+}).then(null, function (error) {
+	//error
+});
+
+
+digits.verifyCode({
+	registrationToken: 'eyJsb2dpblZlcmlmaWNhdGlvblJlcXVlc3RJZCI6InV...',
+	code: '196099',
+}).then(function (results) {
+	//{ success: true, phone: '+33648446907'}
+	//{ success: false, phone: '+33648446907', errors: []}
+	console.log(results);
+}).then(null, function (error) {
+	//error
+});
